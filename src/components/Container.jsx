e92595998d035f78001b3d7f362139f5d94de2d7
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2023-10-04 14:28:47 +0530

    Loader added in reactjs component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,32 +1,41 @@
 import { useState, useEffect } from 'react';
+import Loader from './Loader.jsx';
 
 const Container = ({ onUpdate = null }) => {
   const [bg, setBg] = useState('blue');
-
-  console.log('reactjs client side', onUpdate)
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-      console.log(response);
-      onUpdate && onUpdate(response);
+      setIsLoading(false);
     };
 
     fetchData();
   }, []);
 
-//   const fetchData = async () => {
-//     const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-//     console.log(response);
-//   };
+  //   const fetchData = async () => {
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+  //     console.log(response);
+  //   };
 
-//   fetchData();
+  //   fetchData();
 
-  return (
-    <h1 style={{ color: bg, textAlign: 'center' }} onClick={() => setBg('white')}>
-      Container
-    </h1>
-  );
+  if (isLoading) {
+    return <Loader />;
+  } else {
+    return <h1 style={{ color: bg, textAlign: 'center' }} onClick={() => setBg('white')}>
+    Container
+  </h1>
+  }
+
+//   isLoading ? (
+//     <Loader />
+//   ) : (
+//     <h1 style={{ color: bg, textAlign: 'center' }} onClick={() => setBg('white')}>
+//       Container
+//     </h1>
+//   );
 };
 
 export default Container;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Loader = () => {
+  return <div style={{color: 'white', textAlign: "center"}}>Loading ...</div>;
+};
+
+export default Loader;
